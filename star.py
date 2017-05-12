70852b6a278f886ccf497a31dc07a26b14013ef7
ccmmfit <571893228@qq.com>
2017-05-12 15:25:37 +0800

    shangchuan
    record mine

---
+++ star.py
@@ -0,0 +1,13 @@
+#Îå½ÇÐÇ
+from turtle import Turtle
+
+p = Turtle()
+p.speed(3)
+p.pensize(5)
+p.color("black",'yellow')
+
+p.begin_fill()
+for i in range(5):
+	p.forward(200)
+	p.right(144)
+p.end_fill()
